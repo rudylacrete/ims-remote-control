ebf0b96e427eed74e34d99796d48a707e777f8a8
Rudy GRONDIN <rudylacrete@gmail.com>
2016-04-03 18:04:44 +0200

    added authorization and token management

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,127 @@
+// base 64 encode
+var b64array = "ABCDEFGHIJKLMNOP" +
+           "QRSTUVWXYZabcdef" +
+           "ghijklmnopqrstuv" +
+           "wxyz0123456789+/" +
+           "=";
+function encode64(input) {
+    var base64 = "";
+    var chr1, chr2, chr3 = "";
+    var enc1, enc2, enc3, enc4 = "";
+    var i = 0;
+    do {
+        chr1 = input.charCodeAt(i++);
+        chr2 = input.charCodeAt(i++);
+        chr3 = input.charCodeAt(i++);
+        enc1 = chr1 >> 2;
+        enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+        enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+        enc4 = chr3 & 63;
+        if (isNaN(chr2)) {
+            enc3 = enc4 = 64;
+        } else if (isNaN(chr3)) {
+            enc4 = 64;
+        }
+        base64  = base64  +
+            b64array.charAt(enc1) +
+            b64array.charAt(enc2) +
+            b64array.charAt(enc3) +
+            b64array.charAt(enc4);
+        chr1 = chr2 = chr3 = "";
+        enc1 = enc2 = enc3 = enc4 = "";
+    } while (i < input.length);
+    //console.log(base64 );
+    return base64;
+}
+// end
+
+
+
+var CLIENT_ID = "test";
+var CLIENT_SECRET = "test";
+//var TOKEN_URL = "https://localhost:3002/oauth/token";
+var TOKEN_URL = "http://scooterlabs.com/echo.json";
+
+function _generateBasicAuthHeaderValue(username, password) {
+  return "Basic " + encode64(username + ":" + password);
+}
+
+
+
 var WebServiceConnector = function() {
   this.baseUri = "http://scooterlabs.com/echo.json";
+  this._token = null;
 };
   
-WebServiceConnector.prototype._makeRequest = function(path, method, parameters) {
+WebServiceConnector.prototype._makeRequest = function(method, options, callback) {
+  options = options || {};
+  var path = options.path || "";
+  var headers = options.headers || {};
+  var body = options.body || "";
   var request = new XMLHttpRequest();
+  var baseUri = options.baseUri || this.baseUri;
   request.onreadystatechange = function() {
-    console.log(':::::::', request.readyState);
   	if (request.readyState == 4 && (request.status == 200 || request.status === 0)) {
   		console.log("OK ..." + request.responseText);
+      try{
+        var json = JSON.parse(request.responseText);
+        callback && callback(null, json);
+      }
+      catch(e) {
+        callback && callback(null, request.responseText);
+      }
   	}
   };
   // Send the request
-  request.open(method, this.baseUri + path);
-  request.setRequestHeader('Content-Type', 'application/json');
-  request.send();
+  request.open(method, baseUri + path);
+  Object.keys(headers).forEach(function(key) {
+    request.setRequestHeader(key, headers[key]);
+  });
+  if(!(options.headers && options.headers['Content-Type']))
+    request.setRequestHeader('Content-Type', 'application/json');
+  request.send(body);
+};
+
+WebServiceConnector.prototype._getToken = function(callback) {
+  var options = {
+    baseUri: TOKEN_URL,
+    headers: {
+      "Authorization": _generateBasicAuthHeaderValue(CLIENT_ID, CLIENT_SECRET),
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: "grant_type=client_credentials"
+  };
+  this._makeRequest("POST", options, function(error, response) {
+    if(error) return callback(error);
+    this._token = response.access_token;
+    callback(null);
+  });
+};
+
+WebServiceConnector.prototype.callApiEndpoint = function(path, method, body, callback) {
+  var self = this;
+  var options = {
+    path: path,
+    body: body
+  };
+  if(this._token) {
+    this._makeRequest(method, options, callback);
+  }
+  else {
+    this._getToken(function(error) {
+      if(error) return callback(error);
+      self._makeRequest(method, options, callback);
+    });
+  }
 };
 
 WebServiceConnector.prototype.openValve = function(guid) {
-    this._makeRequest("", "GET", {});
+    this.callApiEndpoint("", "GET", "", console.log);
 };
+
+
+
+
 
 var webServiceConnector = new WebServiceConnector();
 
